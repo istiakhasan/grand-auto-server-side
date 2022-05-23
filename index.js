1f97c4babbb49eca5c5008ea8339f19a88a285f6
Istiak <istieak.hasan1996@gmail.com>
2022-05-23 22:24:29 +0600

    payment intent added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 require('dotenv').config()
 app.use(express.json())
+const stripe=require('stripe')(process.env.STRIPE_SECRET)
 const port=process.env.PORT || 4000
 
 
@@ -23,6 +24,18 @@ const run=async()=>{
         await  client.connect()
         const toolsCollections=client.db('grand_auto').collection('tools')
         const orderCollections=client.db('grand_auto').collection('order')
+        //payment intent 
+        app.post('/create-payment-intent', async(req, res) =>{
+            const service = req.body;
+            const price = service.totalPrice;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount : amount,
+              currency: 'usd',
+              payment_method_types:['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+          });
 
         app.get('/tools',async(req,res)=>{
             const result=await toolsCollections.find().toArray()
@@ -31,9 +44,29 @@ const run=async()=>{
 
         app.get('/tools/:id',async(req,res)=>{
             const id=req.params.id 
-            const query={_id:ObjectId(id)}
-            const result=await toolsCollections.findOne(query)
+            const result=await toolsCollections.findOne({_id:ObjectId(id)})
+         
             res.send(result)
+            // console.log(result,"two")
+            // // const tools=await toolsCollections.find().toArray()
+            //  const order=await orderCollections.find().toArray()
+            //  order.forEach(item=>{
+                
+            //      const toolsOrder=order.filter(items=>items._id==ObjectId(id))
+            //      console.log(toolsOrder,"got it")
+             
+            //  })
+            // // tools.forEach(tool=>{
+            // //     const toolsOrdered=order.filter(items=>items.toolsName===tool.name)
+            // //     const orderQuantity=toolsOrdered.map(item=>item.orderQuantity)
+               
+            // //     const updateQuantity=orderQuantity.reduce((prev,current)=>prev+current,0)
+                
+            // //     tool.available_quantity -= updateQuantity
+            // // })
+           
+            // // const singleItems=tools.find(item=>parseInt(item._id)===parseInt(id))
+            // // res.send(singleItems)
         })
         //book order 
         app.post('/order',async(req,res)=>{
@@ -42,6 +75,39 @@ const run=async()=>{
             res.send(result)
 
         })
+        app.get('/order',async(req,res)=>{
+            const email=req.query.email 
+            const query={email:email}
+            const result=await orderCollections.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/order/:id',async(req,res)=>{
+            const id=req.params.id 
+            const result=await orderCollections.findOne({_id:ObjectId(id)})
+            res.send(result)
+            
+        })
+        app.delete('/order/:id',async(req,res)=>{
+            const id=req.params.id 
+            const result=await orderCollections.deleteOne({_id:ObjectId(id)})
+            res.send(result)
+        })
+        //available order
+        app.get('/availabletools',async(req,res)=>{
+            const tools=await toolsCollections.find().toArray()
+            const order=await orderCollections.find().toArray()
+            tools.forEach(tool=>{
+                const toolsOrdered=order.filter(items=>items.toolsName===tool.name)
+                const orderQuantity=toolsOrdered.map(item=>item.orderQuantity)
+               
+                const updateQuantity=orderQuantity.reduce((prev,current)=>prev+current,0)
+                
+                tool.available_quantity -= updateQuantity
+            })
+            res.send(tools)
+          
+        })
+       
 
     }finally{
 
